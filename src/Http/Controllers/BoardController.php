f0bc2738506cda56c498d1ac6fcc0a979f21906c
Chris Thompson <christhompsontldr@gmail.com>
2016-07-30 11:04:59 -0500

    This project was originally a fork of LaraBB.  Nothing from that project remains and shouldn't have been included in this repo.

--- src/Http/Controllers/BoardController.php
+++ src/Http/Controllers/BoardController.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * This file is part of LaraBB.
- *
- * (c) Jason Clemons <jason@larabb.org>
- *
- * For the full copyright and license information, please view the LICENSE
- * that was distributed with this source code.
- */
 
 namespace Christhompsontldr\Laraboard\Http\Controllers;
 
