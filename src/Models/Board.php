0ee015dc8931e2bb7735dec02960434f09383b1f
Chris Thompson <christhompsontldr@gmail.com>
2016-07-30 11:01:43 -0500

    Removed an unneeded scope.

--- src/Models/Board.php
+++ src/Models/Board.php
@@ -18,44 +18,6 @@ class Board extends Post
         static::addGlobalScope('forumBoard', function(Builder $builder) {
             $builder->where('type', 'Board');
         });
-
-        /**  @todo don't publish  */
-        static::addGlobalScope('privateTeams', function(Builder $builder) {
-            $slug_where = '';
-
-            $team_slugs = [];
-
-            $teams_category = Post::whereSlug('teams')->whereType('Category')->first();
-
-            if (empty($teams_category)) {
-                return;
-            }
-
-            //  guests can't see any teams
-            if (\Auth::guest()) {
-                $builder->where('lft', '<', $teams_category->lft)
-                        ->orWhere('rgt', '>', $teams_category->rgt);
-            }
-            elseif(!\Auth::user()->hasRole('admin')) {
-                $team_slugs = \Auth::user()->teams->pluck('slug')->toArray();
-
-                //  no teams
-                if (count($team_slugs) == 0) {
-                    $builder->where('lft', '<', $teams_category->lft)
-                            ->orWhere('rgt', '>', $teams_category->rgt);
-                } else {
-                    $team_ids = Post::whereType('Board')->whereIn('slug', $team_slugs)->pluck('id')->toArray();
-
-                    //  no teams
-                    $builder->where('lft', '<', $teams_category->lft)
-                            ->orWhere('rgt', '>', $teams_category->rgt);
-
-                    if (count($team_ids) > 0) {
-                        $builder->orWhereIn('id', $team_ids);
-                    }
-                }
-            }
-        });
     }
 
     public function category()
